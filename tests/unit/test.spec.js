161aecdb467780b50d7e964a8b95651ce485994f
huangjialiang <huangjialiang01@corp.netease.com>
2019-03-06 14:57:06 +0800

    feat:use jest instead of mocha

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -1,53 +1,35 @@
-import {expect} from 'chai'
 import {shallowMount, render, createLocalVue} from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 import DrawerLayoutPlugin from '@/lib'
 import {DrawerLayout} from '@/lib'
 import Test from './test.vue'
 
-describe('Test DrawerLayout Installation', () => {
-  it('install component when passed', () => {
+describe('Installation Test', () => {
+  test('install component by plugin way when passed', () => {
     const localVue = createLocalVue()
     localVue.use(DrawerLayoutPlugin)
     const wrapper = shallowMount(Test, {
       localVue,
       attachToDocument: true
     })
-    expect(wrapper.find(DrawerLayout).is(DrawerLayout)).to.equals(true)
+    expect(wrapper.find(DrawerLayout).is(DrawerLayout)).toBe(true)
   })
-  it('renders drawer with width prop when passed', async () => {
+  test('install component by component way when passed', () => {
+    const localVue = createLocalVue()
+    localVue.component(DrawerLayout.name, DrawerLayout)
+    const wrapper = shallowMount(Test, {
+      localVue,
+      attachToDocument: true
+    })
+    expect(wrapper.find(DrawerLayout).is(DrawerLayout)).toBe(true)
+  })
+})
 
+describe('Props Test', () => {
+  test('renders drawer with width prop when passed', async () => {
     const wrapper = shallowMount(DrawerLayout, {
       propsData: {drawerWidth: 800},
       attachToDocument: true
     })
-    expect(wrapper.find('.drawer-wrap').attributes().style).to.include('width: 800px;')
+    expect(wrapper.find('.drawer-wrap').attributes().style).toMatch('width: 800px;')
   })
 })
-
-// describe('DrawerLayout mounted', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: {msg}
-//     })
-//     expect(wrapper.text()).to.include(msg)
-//   })
-//   it('install component when passed', async () => {
-//     const localVue = createLocalVue()
-//     localVue.use(DrawerLayoutPlugin)
-//     const wrapper = shallowMount(DrawerLayout, {
-//       propsData: {drawerWidth: 800},
-//       attachToDocument: true
-//     })
-//     expect(wrapper.find('.drawer-wrap').attributes().style).to.include('width: 800px;')
-//   })
-//   it('renders drawer with width prop when passed', async () => {
-//
-//     const wrapper = shallowMount(DrawerLayout, {
-//       propsData: {drawerWidth: 800},
-//       attachToDocument: true
-//     })
-//     expect(wrapper.find('.drawer-wrap').attributes().style).to.include('width: 800px;')
-//   })
-// })
