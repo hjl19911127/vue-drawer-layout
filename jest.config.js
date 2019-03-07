5375c6929a71793c073d8557d4dde159746d5431
huangjialiang <huangjialiang01@corp.netease.com>
2019-03-07 15:09:01 +0800

    feat:modify jest config to fit codecov

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,5 @@ module.exports = {
   ],
   testURL: "http://localhost/",
   collectCoverage: true,
-  collectCoverageFrom: ["src/lib/**/*.{js,vue}", "!**/node_modules/**"],
-  coverageReporters: ["html", "text-summary"]
+  collectCoverageFrom: ["src/lib/**/*.{js,vue}", "!**/node_modules/**"]
 };
